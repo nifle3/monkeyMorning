10ab7df9cd648f61da3f854543a96dde866e4ede
nifle3 <kuper116rus.2005@gmail.com>
2024-12-04 23:23:29 +0300

    add: huita

--- sql/scheme.sql
+++ sql/scheme.sql
@@ -1,12 +1,12 @@
 CREATE TABLE MorningPolls (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    file_name TEXT
+    file_name TEXT NOT NULL
 );
 
 CREATE TABLE MorningPollsOption (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    morning_poll_id INTEGER,
-    option_tetxt TEXT,
+    morning_poll_id INTEGER NOT NULL,
+    option_tetxt TEXT NOT NULL,
 
     FOREIGN KEY morning_poll_id REFERENCES MorningPolls(id)
 );
@@ -14,5 +14,5 @@ CREATE TABLE MorningPollsOption (
 
 CREATE TABLE SendedPolls (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    poll_tg_id TEXT
+    poll_tg_id TEXT NOT NULL
 );
